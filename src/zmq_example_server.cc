a4a7c064298326039822f4dd4d916ed605b3ca48
Yunlong Liu <davislong198833@gmail.com>
2024-05-15 00:28:55 +0000

    formatter changes

--- src/zmq_example_server.cc
+++ src/zmq_example_server.cc
@@ -6,18 +6,18 @@
 #include <unistd.h>
 #include <zmq.h>
 
-int main(void) {
+int main(int argc, char** argv) {
   //  Socket to talk to clients
-  void *context = zmq_ctx_new();
-  void *responder = zmq_socket(context, ZMQ_REP);
+  void* context = zmq_ctx_new();
+  void* responder = zmq_socket(context, ZMQ_REP);
   int rc = zmq_bind(responder, "tcp://*:5555");
   assert(rc == 0);
 
-  while (1) {
+  while (true) {
     char buffer[10];
     zmq_recv(responder, buffer, 10, 0);
     printf("Received Hello\n");
-    sleep(1); //  Do some 'work'
+    sleep(1);  //  Do some 'work'
     zmq_send(responder, "World", 5, 0);
   }
   return 0;
